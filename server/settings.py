f5a1dda01d50f566137eebd2c5df5da900cca3fb
Bogdan Marukhelin <b.marukhelin@betplanet.com>
2021-06-17 22:18:45 +0300

    update

--- server/settings.py
+++ server/settings.py
@@ -130,11 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    str(BASE_DIR / 'static'),
-)
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = (
+#     str(BASE_DIR / 'static'),
+# )
 
 
 # Default primary key field type
